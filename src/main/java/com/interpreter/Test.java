1e0741ebcfccf155126522e665b41ab2971b8f5d
renguoqing <1002674966@qq.com>
2018-07-24 14:32:38 +0800

    提交文件

--- src/main/java/com/interpreter/Test.java
+++ src/main/java/com/interpreter/Test.java
@@ -5,9 +5,9 @@ package com.interpreter;
  */
 public class Test {
     public static void main(String[] args) {
-        // 计算9+2-8的值
-        int result = new Minus().interpret((new Context(new Plus()  
-                .interpret(new Context(9, 2)), 8)));  
+        // 计算9*2/1-2的值
+        int result = new Minus().interpret(new Context(new Divide().interpret(new Context(new Plus()  
+                .interpret(new Context(9, 2)), 1)),2));  
         System.out.println(result);
     }
 }
